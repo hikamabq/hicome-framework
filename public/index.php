f7c750c0a4634b09df812aeebf44b3e6fac0308b
Hikam Abqory <hikamabqory@Hikams-MacBook-Pro.local>
2023-09-19 18:56:27 +0700

    update template

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require_once '../vendor/hikamabq/hicome/init.php';
 require_once '../app/db.php';
 require_once '../config.php';
-define('base_url', $config['base_url']);
-define('url', $config['base_url'].'/public/');
+define('url', $config['base_url']);
+define('asset', $config['base_url'].'/public/');
 
 $route = new Route();
